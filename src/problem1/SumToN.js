32ef3d9b2c790033737dae073bcb1229a7bb5fbb
rushilramesh <rushilramesh@gmail.com>
2022-11-19 18:23:25 +0800

    Solve Problems

--- src/problem1/SumToN.js
+++ src/problem1/SumToN.js
@@ -5,10 +5,9 @@ var sum_to_n_a = function(n) {
     return sum;
 };
 
-// Recursive solution
+// Recursive solution (Tail recursion not used as it is not space optimised in javascript)
 var sum_to_n_b = function(n) {
     if (n <= 1) return n;
-
     return n + sum_to_n_b(n - 1);
 };
 
@@ -18,9 +17,9 @@ var sum_to_n_c = function(n) {
 };
 
 var test = function() {
-    console.log(sum_to_n_a(2000000));
-    console.log(sum_to_n_b(50));
-    console.log(sum_to_n_c(2000000));
+    console.log(sum_to_n_a(100));
+    console.log(sum_to_n_b(100));
+    console.log(sum_to_n_c(Math.sqrt(Number.MAX_SAFE_INTEGER) / 2));
 }
 
 test();
